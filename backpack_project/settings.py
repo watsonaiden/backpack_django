28ed42174523317d6f48c93ea59e91dd2e48aae7
watsonaiden <awats1819@gmail.com>
2020-10-09 23:21:57 -0600

    added basic formating to folders view. As well as adding more to the base.html file.

--- backpack_project/settings.py
+++ backpack_project/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('templates/static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
