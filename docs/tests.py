c772a83ece9266d9e791931fc6a9a6b3f8ab930c
watsonaiden <awats1819@gmail.com>
2020-10-23 22:31:36 -0600

    updated testing for both user and folder create to work with ajax

--- docs/tests.py
+++ docs/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.test import Client
+from .models import Folder
+from django.urls import reverse
+import json
 # Create your tests here.
+
+
+class test_folderCreate(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': '12345'}
+        User.objects.create_user(**self.credentials)
+
+    def test_folder(self):
+        c = Client()
+        logged_in = c.login(username='testuser', password='12345')
+        self.assertTrue(logged_in)
+        response = c.post(
+            reverse('create_folder'),
+            data={
+            'title': 'testFolder',
+            'desc': ''},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
+        
+        content = json.loads(response.content)
+        value = content['success']
+        self.assertEqual(value, 1)
+        
+    
--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .forms import SignUpForm
+from django.urls import reverse
+import json
 # Create your tests here.
 
 
@@ -21,12 +23,15 @@ class test_signup(TestCase):
                      
 class test_Login(TestCase):
     def setUp(self):
-        self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
-        User.objects.create_user(**self.credentials)
+        self.user = User.objects.create_user(username='testuser', password='12345')
     def test_login(self):
-        response = self.client.post('/accounts/login', self.credentials, follow=True)
-        self.assertTrue(response.context['user'].is_active)
-        last_url, status_code = response.redirect_chain[-1]
-        self.assertEqual(last_url, '/')
+        response = self.client.post(
+            reverse('login_auth'),
+            data={
+            'username': 'testuser',
+            'password': '12345'},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
+        content = json.loads(response.content)
+        value = content['success']
+        self.assertEqual(value, 1)
