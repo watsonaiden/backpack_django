6728913baff6bbfee26e4232af9ae4fd2ae491d6
watsonaiden <awats1819@gmail.com>
2020-10-31 21:18:50 -0600

    added basic tag to report back 3 last accessed folders

--- templates/template_tags/tags.py
+++ templates/template_tags/tags.py
@@ -10,3 +10,12 @@ def current_time(format_string):
     now = datetime.now()
     current_time = now.strftime(format_string)
     return current_time
+
+@register.inclusion_tag("top3.html", takes_context=True)
+def get_top3_folders(context):
+    request = context['request']
+    user = request.user
+    queryset = Folder.objects.filter(user_owner = user).order_by('last_access')[:5]
+        
+    return {"folder_queryset":queryset}
+    
