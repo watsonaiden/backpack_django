873730d97165f494e6f605b00be419687448bda5
watsonaiden <awats1819@gmail.com>
2020-11-08 17:08:21 -0700

    created css frontend and python backend for create reminder button in reminder app

--- reminders/urls.py
+++ reminders/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import ReminderList
+from .views import ReminderList,ajax_createReminder
 
 urlpatterns = [
     path('', ReminderList.as_view(), name='reminders'),
-
+    path('ajax/reminder_create', ajax_createReminder, name="create_reminder"),
 ]
--- reminders/views.py
+++ reminders/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Reminder
 from django.db.models import Q
+from django.db import IntegrityError
+from django.http import JsonResponse
+
 # Create your views here.
 
 
@@ -13,4 +16,24 @@ class ReminderList(ListView):
             return qs
         else:
             return 0
-    
+
+
+def ajax_createReminder(request):
+    if request.method == 'POST' and request.is_ajax():
+        title = request.POST.get('title')
+        description = request.POST.get('desc')
+        deadline = request.POST.get('deadline')
+        try:
+            reminder = Reminder.objects.create(title=title, description=description,
+                                    deadline=deadline, user_owner=request.user)
+            return JsonResponse({"success":1,
+                                 "pk": reminder.pk})
+        except IntegrityError:
+            return JsonResponse({"success":0})
+    return JsonResponse({"success":0})
+            
+
+        
+        
+
+
